02a0a02804b6f1e527fd776dda784062e19198b8
scwanniarachchi <87659257+scwanniarachchi@users.noreply.github.com>
2023-01-31 21:18:13 +0530

    unit test added

--- src/app/Pipes/filter.pipe.spec.ts
+++ src/app/Pipes/filter.pipe.spec.ts
@@ -1,6 +1,40 @@
 import { FilterPipe } from './filter.pipe';
 
 describe('FilterPipe', () => {
+  let pipe = new FilterPipe;
+  let result;
+
+  it('Transform Returns Array Without a Object', () => {
+    let value: never[] = [];
+    
+    result = pipe.transform([], "1234");
+    expect(result).toEqual([]);
+  });
+
+  it('Transform Returns Array with a Object 01', () => {
+    result = pipe.transform([{name: '1234'}], "1234");
+
+    expect(result).toEqual([{name: '1234'}]);
+  });
+
+  it('Transform Returns Array with Objects 02', () => {
+    result = pipe.transform([{name: '1234'}, {name: '1246567'}, {name: '14'}], "1");
+
+    expect(result).toEqual([{name: '1234'}, {name: '1246567'}, {name: '14'}]);
+  });
+
+  it('Transform Returns Array with Objects 03', () => {
+    result = pipe.transform([{name: '1234'}, {name: '1246567'}, {name: '14'}, {name: '2222'}], "4");
+
+    expect(result).toEqual([{name: '1234'}, {name: '1246567'}, {name: '14'}]);
+  });
+
+  it('Transform Returns Empty Array without any Object 04', () => {
+    result = pipe.transform([{name: '1234'}, {name: '1246567'}, {name: '14'}, {name: '2222'}], "9");
+
+    expect(result).toEqual([]);
+  });
+
   it('create an instance', () => {
     const pipe = new FilterPipe();
     expect(pipe).toBeTruthy();
